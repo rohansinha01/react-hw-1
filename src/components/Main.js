c6e57fede4e8a50229a59e31c3bc013c864e2b7f
rohansinha01 <rohansinha01@gmail.com>
2024-01-23 23:50:25 -0500

    all pieces are on page

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,9 @@ import Projects from "./Projects.js"
 const Main = (props) => {
 
     //The Components Returned JSX
-    return <div className="Main"> <Projects/></div>
+    return <div className="Main"> 
+    <Projects project="1" title="Cliché Bank" description="Quote Generator"/>
+    <Projects project="2" title="Karaoke List" description="Track what Karaoke songs you have sang or want to sing."/></div>
 }
 
 // export the component
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,12 @@
 const Projects = (props) => {
 
     //The Components Returned JSX
-    return <div className="Projects"> This is Projects</div>
+    return <div className="Projects">
+        <h2>Project {props.project}</h2>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+
+    </div>
 }
 
 // export the component
