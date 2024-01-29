b4e182b1341ee468687bd78fb2b755ca1bfc9683
rohansinha01 <rohansinha01@gmail.com>
2024-01-29 17:39:37 -0500

    updated css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ const Footer = (props) => {
     //The Components Returned JSX
     return <div className="Footer"> 
     <p id="footer-description">Rohan Sinha can be reached for freelance work by <br></br>emailing rohansinha01@gmail.com</p>
-
+    <div className="social"><span><a  href="facebook.com">Facebook</a></span>
+    <span><a href="twitter.com">Twitter</a></span>
+    <span><a href="instagram.com">Instagram</a></span></div>
     </div>
 }
 
